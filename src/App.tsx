0e9f902ab3915f0fb31e47937f6abf8455f49f40
Adassa2101 <90108698+Adassa2101@users.noreply.github.com>
2025-07-27 13:42:13 +0300

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css'
+import Header from "./components/Header.jsx";
+import Main from "./components/Main.jsx";
+import Footer from "./components/Footer.jsx";
+import {useState} from "react";
+import {navItems} from "./utils/constants.js";
+import {SWContext} from "./utils/context.ts";
+
+function App() {
+    const [page,setPage] = useState(navItems[0]);
+    return (
+        <div >
+            <SWContext value={{page, changePage:setPage}}>
+            <Header changePage={setPage}/>
+            <Main />
+            <Footer/>
+            </SWContext>
+        </div>
+
+    )
+}
+
+export default App
---
+++ src/components/StarWars.tsx
@@ -0,0 +1,12 @@
+import {starWarsInfo} from "../utils/constants.js";
+import Text from "./ui/Text.jsx";
+
+
+const StarWars = () => {
+    return (
+        <Text>{starWarsInfo}</Text>
+    );
+};
+
+
+export default StarWars;
