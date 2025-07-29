127a64172175bad3e606d2504136b63054577662
Adassa2101 <90108698+Adassa2101@users.noreply.github.com>
2025-07-29 10:08:03 +0300

    Complete static routing

---
+++ src/components/ErrorPage.tsx
@@ -0,0 +1,13 @@
+import Text from "./ui/Text.tsx";
+
+
+const ErrorPage = () => {
+    return (
+        <Text>
+            {'O-o-ps, something went wrong'}
+        </Text>
+    );
+};
+
+
+export default ErrorPage;
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,24 +3,24 @@ import AboutMe from "./AboutMe.jsx";
 import StarWars from "./StarWars.jsx";
 import Contact from "./Contact.jsx";
 import {navItems} from "../utils/constants.js";
-import {SWContext} from "../utils/context.js";
-import {useContext} from "react";
+import {Route, Routes} from "react-router";
+import ErrorPage from "./ErrorPage.tsx";
 
 
 const Main = () => {
-    const {page} = useContext(SWContext);
 
 
-    switch (page) {
-        case navItems[1]:
-            return <AboutMe/>;
-        case navItems[2]:
-            return <StarWars/>;
-        case navItems[3]:
-            return <Contact/>;
-        default:
-            return <Home/>;
-    }
+    return (
+        <Routes>
+            {['/', navItems[0]].map(path => <Route key = {path} path={path}element={<Home/>}/>)}
+            <Route path={`${navItems[1]}`} element={<AboutMe/>}/>
+            <Route path={`${navItems[2]}`} element={<StarWars/>}/>
+            <Route path={`${navItems[3]}`} element={<Contact/>}/>
+            <Route path={'*'} element={ <ErrorPage/>}/>
+
+        </Routes>
+    )
+
 }
 
 
